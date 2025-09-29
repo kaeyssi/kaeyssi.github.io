3fd4f6b187d8b06cbfb65ffbac2d28927ad3c0f8
kaeyssi <kennethcedricsantilla@gmail.com>
2025-09-30 01:48:53 +0800

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,46 @@
+
+    document.getElementById("header").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("exam").style.display = "none";
+    document.getElementById("activity").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+
+function home(){
+    document.getElementById("header").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("exam").style.display = "none";
+    document.getElementById("activity").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+}
+function project(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("header").style.display = "";
+    document.getElementById("project").style.display = "";
+    document.getElementById("exam").style.display = "none";
+    document.getElementById("activity").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+  }
+function exam(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("header").style.display = "";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("exam").style.display = "";
+    document.getElementById("activity").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+  }
+function activity(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("header").style.display = "";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("exam").style.display = "none";
+    document.getElementById("activity").style.display = "";
+    document.getElementById("quiz").style.display = "none";
+  }
+function quiz(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("header").style.display = "";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("exam").style.display = "none";
+    document.getElementById("activity").style.display = "none";
+    document.getElementById("quiz").style.display = "";
+  }
